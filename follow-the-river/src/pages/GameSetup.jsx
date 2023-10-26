554bdb39de8f764c63f67941b02f7c1b93b51c34
Raymond Bach <raymondmsbach@gmail.com>
2023-10-25 20:15:42 -0500

    React Router
    Set up routing, and pages

---
+++ follow-the-river/src/pages/GameSetup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GameSetup = () => {
+  return (
+    <h1>Game Set Up</h1>
+  );
+}
+
+export default GameSetup;
---
+++ follow-the-river/src/pages/HowToPlay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HowToPlay = () => {
+  return (
+    <h1>How to play</h1>
+  );
+}
+
+export default HowToPlay;
